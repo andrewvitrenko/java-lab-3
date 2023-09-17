6bd29092914a5b8acb6c13989497b2d6a03bb24d
andrewvitrenko <avitrenko2004@gmail.com>
2023-09-17 13:29:06 +0200

    fix: - renamed methods - refactored code

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,30 @@ import java.util.Arrays;
 
 public class Main {
     static final String example = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ullamcorper facilisis enim, vel bibendum leo scelerisque ut. Nunc ac massa ac nulla sagittis pharetra id eget ipsum. In hac habitasse platea dictumst. Sed eget bibendum ligula. Aliquam ultricies sed odio ut porta. Aenean diam justo, hendrerit auctor nulla id, molestie mattis metus. Quisque eu nisl vitae magna laoreet elementum. Nulla cursus, arcu nec dapibus gravida, turpis sem feugiat est, non laoreet sapien odio quis mauris. In vel neque at justo auctor vulputate. Praesent malesuada urna sed erat eleifend, eget faucibus enim egestas. Phasellus interdum a tellus a posuere. Maecenas quis lectus sit amet lectus elementum blandit.";
+    static final String WORDS_SEPARATOR = ",?\\s";
+    static final String SENTENCE_SEPARATOR = "[\\.!?]\\s";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StringBuilder stringBuilder = new StringBuilder(example);
 
-        String[] sentences = getSentences(stringBuilder, "[\\.!?]\\s");
+        String[] sentences = getSentences(stringBuilder, SENTENCE_SEPARATOR);
 
-        String[] result = findWords(sentences);
+        validateText(sentences);
+        validateSentence(sentences[0]);
 
-        System.out.printf("result - %s\n", Arrays.toString(result));
+        String[] result = findWords(sentences[0], sentences);
+
+        if (result.length > 0) {
+            System.out.printf("result - %s\n", Arrays.toString(result));
+        } else {
+            System.out.println("0 words found");
+        }
     }
 
-    public static String[] findWords(String[] sentences) {
+    public static String[] findWords(String base, String[] sentences) {
         ArrayList<String> result = new ArrayList<>();
 
-        String firstSentence = sentences[0];
-        String[] words = getWords(firstSentence, ",?\\s");
+        String[] words = getWords(base, WORDS_SEPARATOR);
 
         for (String word : words) {
             Object[] matches = Arrays.stream(sentences).filter(sentence -> sentence.contains(word)).toArray();
@@ -37,5 +45,20 @@ public class Main {
 
     public static String[] getWords(String source, String pattern) {
         return source.split(pattern);
+    }
+
+    public static void validateText(String[] sentences) throws Exception {
+        checkLength(sentences, 2, "Not enough text");
+    }
+
+    public static void validateSentence(String sentence) throws Exception {
+        String[] words = sentence.split(WORDS_SEPARATOR);
+        checkLength(words, 1, "First sentence should contain at least 1 word");
+    }
+
+    public static void checkLength(String[] entities, int length, String errorMessage) throws Exception {
+        if (entities.length < length) {
+            throw new Exception(errorMessage);
+        }
     }
 }
